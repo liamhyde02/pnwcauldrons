1ec03e7b58f21dac7b4b48413725e22b7a5e3f8a
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2024-04-22 21:57:57 -0700

    made sure my reset restarts the ledger

--- src/api/admin.py
+++ src/api/admin.py
@@ -21,17 +21,21 @@ def reset():
     gold_reset_sql = "DELETE FROM gold_ledger"
     carts_sql = f"DELETE FROM carts"
     cart_items_sql = f"DELETE FROM cart_items"
-    starting_gold_sql = "INSERT INTO gold_ledger (order_id, gold) VALUES (-1, 100)"
+    starting_gold_sql = "INSERT INTO gold_ledger (order_id, gold) VALUES (:order_id, 100)"
     capacity_reset_sql = "DELETE FROM global_plan"
-    capacity_restart_sql = "INSERT INTO global_plan (order_id, potion_capacity_units, ml_capacity_units) VALUES (-1, 1, 1)"
+    starting_capacity_sql = "INSERT INTO global_plan (order_id, potion_capacity_units, ml_capacity_units) VALUES (:order_id, 1, 1)"
+    processed_entry_sql = "INSERT INTO processed (order_id, type) VALUES (:order_id, 'reset') RETURNING id"
     with db.engine.begin() as connection:
         connection.execute(sqlalchemy.text(potion_reset_sql))
         connection.execute(sqlalchemy.text(barrel_reset_sql))
         connection.execute(sqlalchemy.text(gold_reset_sql))
         connection.execute(sqlalchemy.text(carts_sql))
         connection.execute(sqlalchemy.text(cart_items_sql))
-        connection.execute(sqlalchemy.text(starting_gold_sql))
+        id = connection.execute(sqlalchemy.text(processed_entry_sql), [{"order_id": -1}]).scalar_one()
+        connection.execute(sqlalchemy.text(starting_gold_sql),
+                           [{"order_id": id}])
         connection.execute(sqlalchemy.text(capacity_reset_sql))
-        connection.execute(sqlalchemy.text(capacity_restart_sql))
+        connection.execute(sqlalchemy.text(starting_capacity_sql),
+                           [{"order_id": id}])
     return "OK"
 
