a576945e99ac71eb48fae24ceeba727c18520297
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2024-05-01 19:38:52 -0700

    I think I was training on test data which might be skewing things, restarting

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -13,7 +13,7 @@ def get_catalog():
     """
     potion_quantity_sql = "SELECT potion_type, SUM(quantity) as potion_quantity FROM potions GROUP BY potion_type having SUM(quantity) > 0"
     potion_catalog_sql = "SELECT * FROM potion_catalog_items WHERE potion_type = :potion_type"
-    visits_sql = "SELECT character_class, COUNT(character_class) as total_characters FROM visits JOIN global_time ON visits.day = global_time.day GROUP BY character_class"
+    visits_sql = "SELECT character_class, COUNT(character_class) as total_characters FROM visits JOIN global_time ON visits.day = global_time.day GROUP BY character_class ORDER BY total_characters DESC LIMIT 6"
     class_preference_sql = "SELECT potion_type, COALESCE(COUNT(potion_type), 0) as amount_bought FROM class_preferences WHERE character_class = :character_class GROUP BY potion_type, character_class"
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text(potion_quantity_sql))
@@ -25,39 +25,39 @@ def get_catalog():
 
         catalog = []
         listed_items = 0
-        for character_class in sorted_classes:
-            class_preference = connection.execute(sqlalchemy.text(class_preference_sql), 
-                                                  [{"character_class": character_class}]).fetchall()
-            class_preference = [row._asdict() for row in class_preference]
-            if len(class_preference) == 0:
-                print(f"character_class: {character_class}, class_preference: No preference yet")
-            else: 
-                weighted_choices = [(pref['potion_type'], pref['amount_bought']) for pref in class_preference]
-                selected_potion = random.choices(
-                    [choice[0] for choice in weighted_choices], 
-                    weights=[choice[1] for choice in weighted_choices],
-                    k=1
-                )[0]
-                print(f"character_class: {character_class}, class_preference: {class_preference}, selected_potion: {selected_potion}")
-                for potion in potions:
-                    if potion["potion_type"] == selected_potion:
-                        catalog_entry = connection.execute(sqlalchemy.text(potion_catalog_sql), 
-                                                    [{"potion_type": potion["potion_type"]}]).fetchone()._asdict()
-                        catalog.append(
-                            {
-                                "sku": catalog_entry["sku"],
-                                "name": catalog_entry["name"],
-                                "quantity": potion["potion_quantity"],
-                                "price": catalog_entry["price"],
-                                "potion_type": catalog_entry["potion_type"],
-                            }
-                        )
-                        listed_items += 1
-                        potions.remove(potion)
-            if listed_items >= 6:
-                break
-            if len(potions) == 0:
-                break
+        # for character_class in sorted_classes:
+        #     class_preference = connection.execute(sqlalchemy.text(class_preference_sql), 
+        #                                           [{"character_class": character_class}]).fetchall()
+        #     class_preference = [row._asdict() for row in class_preference]
+        #     if len(class_preference) == 0:
+        #         print(f"character_class: {character_class}, class_preference: No preference yet")
+        #     else: 
+        #         weighted_choices = [(pref['potion_type'], pref['amount_bought']) for pref in class_preference]
+        #         selected_potion = random.choices(
+        #             [choice[0] for choice in weighted_choices], 
+        #             weights=[choice[1] for choice in weighted_choices],
+        #             k=1
+        #         )[0]
+        #         print(f"character_class: {character_class}, class_preference: {class_preference}, selected_potion: {selected_potion}")
+        #         for potion in potions:
+        #             if potion["potion_type"] == selected_potion:
+        #                 catalog_entry = connection.execute(sqlalchemy.text(potion_catalog_sql), 
+        #                                             [{"potion_type": potion["potion_type"]}]).fetchone()._asdict()
+        #                 catalog.append(
+        #                     {
+        #                         "sku": catalog_entry["sku"],
+        #                         "name": catalog_entry["name"],
+        #                         "quantity": potion["potion_quantity"],
+        #                         "price": catalog_entry["price"],
+        #                         "potion_type": catalog_entry["potion_type"],
+        #                     }
+        #                 )
+        #                 listed_items += 1
+        #                 potions.remove(potion)
+        #     if listed_items >= 6:
+        #         break
+        #     if len(potions) == 0:
+        #         break
         
         random.shuffle(potions)
         print(f"remaining potions: {potions}")
